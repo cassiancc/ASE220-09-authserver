2a3534f7c7d70216dd60406d06ec10c80d591e3a
Cassian Godsted <cassian@godsted.com>
2024-03-16 18:44:05 -0400

    Function based sign in.
    Now gives user a chance to re-enter password.

--- signin.js
+++ signin.js
@@ -3,32 +3,41 @@ const validator = require('validator');
 const fs = require('fs');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-rl.question('Enter your email address: ', (email) => {
 
-  rl.question('Enter your password: ', (password) => {
 
-    const user = {
-      email,
-      password 
-    };
+function signIn() {
+    //Check email
+    rl.question('Enter your email address: ', function (email) {
+        //Check password
+        rl.question('Enter your password: ', async function (password) {
 
-    fs.readFile("credentials.json", "utf8", (error, data) => {
-        if (error) {
-          console.log(error);
-          return;
-        }
-        data = JSON.parse(data);
-        if ((user.email == data.email) && (user.password == data.password)) {
-            console.log("Welcome!")
-        }
-        else {
-            console.log("Your email or password is incorrect. Please try again.")
-        }
+            const user = {
+                email,
+                password
+            };
+
+            checkLogin(user)
+        });
+    });
+}
+
+async function checkLogin(user) {
+    data = fs.readFileSync("credentials.json", "utf8")
+    data = JSON.parse(data);
+    //If provided email matches, close "loop".
+    if ((user.email == data.email) && (user.password == data.password)) {
+        console.log("Welcome!")
         rl.close();
-      });
-  });
-});
+    }
+    //If provide email does not match, check again.
+    else {
+        console.log("Your email or password is incorrect. Please try again.")
+        signIn()
+    }
+}
+
+signIn()

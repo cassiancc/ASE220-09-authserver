a1b2c3c8c9dab0bad13dd1805bb370d605aa3a18
Christian Lane <christianm3870@hotmail.com>
2024-03-18 19:48:20 -0400

    finished server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const port = 5050; // we can change this
 
 const server = http.createServer((req, res)=> {
-    const { headers, method, url } = req;
+    const { method, url } = req;
     if (method === 'GET') {
         res.writeHead(200, {
             'Content-Type':'text/html'
@@ -18,43 +18,69 @@ const server = http.createServer((req, res)=> {
         let body = [];
         req.on('data', function(data) {
             body.push(data);
-        })
-        .on("end", function() {
-            body = JSON.parse(Buffer.concat(body).toString());
 
+        }).on("end", function() {
+            body = JSON.parse(Buffer.concat(body).toString());
+            let data = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+            const email= body["email"].toString();
+            const password = body['password'].toString();
+            const index = data.map(e => { return e.email }).indexOf(email);
+            let unauthorized = false;
             res.on('error', error => {
                 console.error(error);
             })
             if (url.includes('/api/signup')) {
                 //signup stuff
+                if(index === -1) {
+                    data.push(body)
+                    fs.writeFileSync('credentials.json', JSON.stringify(data, null, 2))
+                    console.log("User created successfully.");
+                    res.writeHead(200, {
+                        "Content-Type": "application/json"
+                    });
+                    res.end(JSON.stringify({status:1, message:"Signup Successful"}));
+                    
+                } else {
+                    unauthorized = true;
+                    console.error("That username already exists.");
+                }
                 
                 
             } else if (url.includes('/api/signin')) {
                 //signin stuff
-                let data = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
-                
-                const email= body["email"].toString();
-                const password = body['password'].toString();
-                
-                index = data.map(function(e) { return e.email }).indexOf(email)
-                console.log(index);
-                if(index !== null) {
-                    
+                if(index !== -1) {
                     if(data[index].password === password) {
-                        console.log("Login Successful.");
+                        //do successful login things
+                        console.log("Signin Successful.");
+                        res.writeHead(200, {
+                            "Content-Type": "application/json"
+                        });
+                        res.end(JSON.stringify({
+                            status:1, message:"Signin Successful"
+                        }));
+                        
+                    } else {
+                        unauthorized = true;
+                        console.log("The password was incorrect.");
                     }
                 } else {
+                    unauthorized = true;
                     console.log("Username not present.");
                 }
-    
+            }
+            if(unauthorized) {
+                res.writeHead(400, {"Content-Type":"application/json"});
+                res.end(JSON.stringify({status:-1, error:"Unauthorized Request"}));
             }
         })
         
     }
-    res.end();
 });
 
 server.listen(port, () => console.log(`Server Listening on port ${port}`))
 
-// use below curl command to test login functionality
+// use below curl command to test sign in functionality
 //curl -i -X POST -d '{"email":"test@test.com", "password":"test123"}' localhost:5050/api/signin
+
+// use below curl command to test sign up functionality
+//curl -i -X POST -d '{"email":"new@email.com", "password":"password"}' localhost:5050/api/signup
